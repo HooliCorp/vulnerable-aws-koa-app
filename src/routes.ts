a16047bd93290fba2bf839ed9983a27132c7ae50
Prabhu Subramanian <prabhu@shiftleft.io>
2021-06-09 21:32:27 +0000

    v3 sdk

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,19 @@ import { Context } from "koa";
 import Router from "koa-router";
 import userRoutes from "./users";
 import storageRoutes from "./storage";
+import storageV3Routes from "./storagev3";
 
 const router = new Router();
 
-router.use("/api/v1/users", userRoutes.routes(), userRoutes.allowedMethods());
+router.use("/api/v2/users", userRoutes.routes(), userRoutes.allowedMethods());
 router.use(
-  "/api/v1/storage",
+  "/api/v2/storage",
   storageRoutes.routes(),
   storageRoutes.allowedMethods()
 );
-
+router.use(
+  "/api/v3/storage",
+  storageV3Routes.routes(),
+  storageV3Routes.allowedMethods()
+);
 export default router;
